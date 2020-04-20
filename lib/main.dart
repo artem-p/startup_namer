771e0b468272d97799ca6be45e5955d1b403ce50
Artyom Pugachev <artempugachev1@gmail.com>
2020-04-20 19:16:29 +0300

    add favorites screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>(); 
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
-
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
 
-        return _buildRow(_suggestions[index]);
-      });
+          return _buildRow(_suggestions[index]);
+        });
   }
 
-  
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
 
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont
-      ),
-
+      title: Text(pair.asPascalCase, style: _biggerFont),
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
-
       onTap: () {
         setState(() {
           if (alreadySaved) {
@@ -62,11 +54,27 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-
   void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
 
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+        appBar: AppBar(title: Text("Saved Suggestions")),
+        body: ListView(children: divided),
+      );
+    }));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -77,12 +85,10 @@ class RandomWordsState extends State<RandomWords> {
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
         ],
       ),
-
       body: _buildSuggestions(),
     );
   }
 }
-
 
 class RandomWords extends StatefulWidget {
   @override
